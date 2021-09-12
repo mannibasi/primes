0cbdcfd8641420684573c1275be4c39ab640227e
Manni Basi <manni.basi@gmail.com>
2021-09-12 11:29:07 +0100

    Added failing test for sum of primes with empty list

--- test_primes.py
+++ test_primes.py
@@ -6,5 +6,8 @@ def test_prime_low_number():
 def test_prime_prime_number():
     assert is_prime(29)
 
-def test_prime_composite_numbeer():
+def test_prime_composite_number():
     assert is_prime(15) == False
+
+def test_sum_of_primes_empty_list():
+    assert sum_of_primes([]) == 0
